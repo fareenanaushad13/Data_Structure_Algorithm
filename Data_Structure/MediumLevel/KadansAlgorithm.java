9bdf49163f88afcefdb76cdba84b5b1637b28d2f
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2025-06-07 21:48:00 +0530

    Kadan's Algorithm

---
+++ Data_Structure/MediumLevel/KadansAlgorithm.java
@@ -0,0 +1,18 @@
+public class KadansAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubarraySum(arr));
+    }
+
+    public static int maxSubarraySum(int[] arr) {
+        int maxSoFar = arr[0];
+        int currMax = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currMax = Math.max(arr[i], currMax + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currMax);
+        }
+
+        return maxSoFar;
+    }
+}
