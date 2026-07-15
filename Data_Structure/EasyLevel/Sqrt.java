9d5cbe89145d866a6aaa1ecae509445d736f5fa4
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-07-16 01:11:08 +0530

    Sqrt(x)

---
+++ Data_Structure/EasyLevel/Sqrt.java
@@ -0,0 +1,39 @@
+public class Sqrt{
+      public int mySqrt(int x) {
+
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+public static void main(String[] args) {
+
+        Sqrt obj = new Sqrt();
+
+        int x = 8;
+
+        int result = obj.mySqrt(x);
+
+        System.out.println("Square root of " + x + " = " + result);
+    }
+}
