94fc69739cfe5b3559beb99e82a668e3348fe532
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-08-02 00:50:18 +0530

    Fibonacci Number

---
+++ Data_Structure/EasyLevel/FibonacciNumber.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class FibonacciNumber {
+    static int[] dp;
+
+    public static int fib(int n){
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return solve(n);
+    }
+
+    public static int solve(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = solve(n-1) + solve(n-2);
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n:");
+        int n  = sc.nextInt();
+
+        System.out.println("Fibonacci Number : " +  fib(n));
+
+        sc.close();
+    }
+}
