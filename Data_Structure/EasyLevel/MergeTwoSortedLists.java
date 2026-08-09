21ab50baa8d09196014fa776fab6517967624635
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-08-10 01:26:39 +0530

    Merge Two sorted Lists Easy

---
+++ Data_Structure/EasyLevel/MergeTwoSortedLists.java
@@ -0,0 +1,59 @@
+public class MergeTwoSortedLists {
+static class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+public static ListNode mergeTwoSortedLists(ListNode list1, ListNode list2){
+    ListNode dummy = new ListNode(0);
+    ListNode current =  dummy;
+
+    while(list1 != null && list2 != null){
+        if(list1.val <= list2.val){
+            current.next = list1;
+            list1 = list1.next;
+        }else{
+            current.next = list2;
+            list2 = list2.next;
+        }
+
+        current = current.next;
+    }
+
+    if(list1 != null){
+        current.next = list1;
+    }else{
+        current.next = list2;
+    }
+
+    return dummy.next;
+}
+
+public static void printList(ListNode head){
+    while (head != null) {
+        System.out.print(head.val + " ");
+        head = head.next;
+    }
+}
+public static void main(String[] args) {
+
+        // list1 = 1 → 2 → 4
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // list2 = 1 → 3 → 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode result = mergeTwoSortedLists(list1, list2);
+        System.out.print("Merge sorted List");
+        printList(result);
+}
+    
+}
