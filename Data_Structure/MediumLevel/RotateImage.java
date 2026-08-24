b7ead7d29883719e27f2010645c6ec6581bbf8e6
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-08-25 02:11:03 +0530

    Rotate Image

---
+++ Data_Structure/MediumLevel/RotateImage.java
@@ -0,0 +1,56 @@
+
+public class RotateImage {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+
+        int n = matrix.length;
+        
+        // Print original matrix
+        System.out.println("Original matrix:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // reverse every row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // print rotated matrix
+        System.out.println("Rotated matrix:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
