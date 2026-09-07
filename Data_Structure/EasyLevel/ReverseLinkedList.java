5955d6a493c9cd717e68387298f9852ded29ce3d
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-09-08 02:04:18 +0530

    Reverse Linked List

---
+++ Data_Structure/EasyLevel/ReverseLinkedList.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            sc.close();
+            return;
+        }
+
+        System.out.println("Enter values:");
+
+        ListNode head = null;
+        ListNode tail = null;
+
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+
+            ListNode newNode = new ListNode(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        head = reverseList(head);
+
+        System.out.println("Reversed list:");
+        printList(head);
+
+         sc.close();
+
+    }
+}
