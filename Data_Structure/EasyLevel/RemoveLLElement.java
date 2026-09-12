9fc0e60d60280684ddcf1e06057c459403df0717
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-09-13 01:34:49 +0530

    Remove Linked List Elements(nodes whose value == val)

---
+++ Data_Structure/EasyLevel/RemoveLLElement.java
@@ -0,0 +1,49 @@
+class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveLLElement {
+    public static ListNode removeElements(ListNode head, int val) {
+        // remove from beginning
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        int val = 6;
+
+        head = removeElements(head, val);
+
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+}
