1a7cee06577a94fcf2675aa453ff5cdb4f8c5e80
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-09-15 02:01:44 +0530

    Swap Nodes in Pairs

---
+++ Data_Structure/MediumLevel/SwapPairsLL.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class SwapPairsLL {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            // swap;
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        ListNode head = null;
+        ListNode tail = null;
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+
+            ListNode newNode = new ListNode(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        head = swapPairs(head);
+        System.out.println("After Swapping:");
+        printList(head);
+        sc.close();
+    }
+}
