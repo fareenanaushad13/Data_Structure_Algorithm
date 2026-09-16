a21af3c784f1729776353414d9fdecccea0ca6cb
fareenanaushad13 <114231603+fareenanaushad13@users.noreply.github.com>
2026-09-17 00:57:01 +0530

    Squares Of a Sorted Array

---
+++ Data_Structure/EasyLevel/SquareOfSortedArray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SquareOfSortedArray{
+    public static int[] sortedSquares(int[] nums){
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n-1;
+
+        for(int i=n-1;i>=0;i--){
+            if(Math.abs(nums[left])>Math.abs(nums[right])){
+                result[i] = nums[left] * nums[left];
+                left++;
+            }else{
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+            
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-6,-1,0,1,3,6};
+        int[] result = sortedSquares(nums);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
